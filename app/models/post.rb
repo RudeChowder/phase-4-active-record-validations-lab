d5e21fc82da61bf1af0c35224a1bc837438ccf77
Stephen Crane <scrane88@gmail.com>
2021-09-10 09:49:34 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbaity?
+
+  def clickbaity?
+    baits = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    if baits.none? { |bait| title&.include? bait }
+      errors.add(:title, "Title is not clickbaity enough")
+    end
+  end
 end
